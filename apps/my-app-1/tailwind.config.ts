cc08b12505e22abcc08d945a1424d52d868e7ff4
2DC <kkts9308@gmail.com>
2024-03-15 14:41:49 +0900

    🔧 공용 tailwind, css config 생성 및 my-app-1에 테스트 반영(성공)

--- apps/my-app-1/tailwind.config.ts
+++ apps/my-app-1/tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  presets: [require("@repo/tailwind-config/shadcn")],
+  presets: [
+    require("@repo/tailwind-config/shadcn"),
+    require("@repo/tailwind-config/extends-shared-colors"),
+  ],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
---
+++ packages/tailwind-config/extends-shared-colors.js
@@ -0,0 +1,33 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      colors: {
+        app: {
+          blue: {
+            "001": "#ebf3ff",
+            "002": "#4674fe",
+            "blue-background": "#fafcff",
+          },
+          gray: {
+            "001": "#ffffff",
+            "002": "#f5f5f5",
+            "003": "#e9eaee",
+            "004": "#cccccc",
+            "005": "#c4c4c4",
+            "006": "#b6b7b9",
+            "007": "#888888",
+          },
+        },
+      },
+    },
+  },
+};
